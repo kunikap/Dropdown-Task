78197ccc1392e4e29971a4ef5a91c0abde31d108
kunika jha <kunika.jha@gmail.com>
2021-02-15 21:27:49 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <Home />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    
       </header>
     </div>
   );
